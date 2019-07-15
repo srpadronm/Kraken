cc025e2009f6eb9a765c6d332e9c9c74a2381aed
Sergio <srpadron@gmail.com>
2019-07-15 20:28:30 +0100

    tres y cuatro

--- Kraken/ViewController.swift
+++ Kraken/ViewController.swift
@@ -14,6 +14,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func tres() {
+        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
+    }
+    
+    func cuantro() {
+        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
+    }
 
 
 }
