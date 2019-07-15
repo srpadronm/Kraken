2ea07ecf0086201aa0285b0de8ef625c0c13a8fa
Sergio <srpadron@gmail.com>
2019-07-15 20:48:08 +0100

    new seis

--- Kraken/ViewController.swift
+++ Kraken/ViewController.swift
@@ -50,6 +50,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func seis() {
+        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
+    }
+    
     func siete() {
     }
 
