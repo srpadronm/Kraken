880f3d715584346d64e72b40fa9ebc985e1e1afb
Sergio <srpadron@gmail.com>
2019-07-15 20:18:37 +0100

    Initial Commit

---
+++ Kraken/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kraken
+//
+//  Created by Sergio Padron on 15/07/2019.
+//  Copyright © 2019 Sergio Padron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
